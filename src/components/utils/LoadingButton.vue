ea98785e06f3e5ecf7f7b9e34ab2faa0e11fc121
Javier Quiroz <javier.quiroz@forgerock.com>
2018-05-14 17:21:38 +0000

    OPENIDM-10900 Create load button component

---
+++ src/components/utils/LoadingButton.vue
@@ -0,0 +1,51 @@
+<template>
+        <button :class="['btn btn-primary d-flex align-items-center', {'disabled': loading}]" type="button" @click="$emit('click')">
+            <span class="align-self-start">
+                {{label}}
+            </span>
+            <span :class="['fr-grow', {'fr-grow-in': loading}]">
+                <transition name="fr-fade">
+                    <clip-loader v-if="loading" :color="'white'" :size="'1rem'" class="position-relative fr-clip-loader ml-3"></clip-loader>   
+                </transition>
+            </span>
+        </button>
+</template>
+<script>
+    import { ClipLoader } from 'vue-spinner/dist/vue-spinner.min.js';
+    export default {
+        name: 'LoadingButton',
+        components: { ClipLoader },
+        props: {
+            label: String,
+            loading: {
+                type: Boolean,
+                default: false
+            }
+        }
+    };
+</script>
+<style lang="scss" scoped>
+    .fr-grow {
+        width: 0;
+        transition: all .25s ease;
+    }
+    .fr-grow-in {
+        width: 3rem;
+    }
+
+    .fr-fade-enter-active {
+        transition: all .125s ease;
+    }
+    .fr-fade-leave {
+        transition: all .125s ease;
+    }
+    .fr-fade-enter, .fr-fade-leave-to {
+        opacity: 0;
+    }
+    .fr-fade-enter-to {
+        opacity: 1
+    }
+    .fr-clip-loader {
+        top: 0.15rem;
+    }
+</style>
