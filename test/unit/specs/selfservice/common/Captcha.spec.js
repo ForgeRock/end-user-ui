d28dc5b2690e0a1cabb43efc67da98feb98b867f
jason.browne <jason.browne@forgerock.com>
2018-05-18 10:25:19 -0700

    OPENIDM-10964 Fix jenkins test failure

--- test/unit/specs/selfservice/common/Captcha.spec.js
+++ test/unit/specs/selfservice/common/Captcha.spec.js
@@ -4,21 +4,23 @@ import VueI18n from 'vue-i18n';
 import BootstrapVue from 'bootstrap-vue';
 import translations from '@/translations';
 import { mount } from '@vue/test-utils';
+import Sinon from 'sinon';
 
 describe('Captcha.vue', () => {
-    var mountWrapper = () => {
-        return mount(Captcha, {
-            i18n,
-            propsData: {
-                apiType: 'username',
-                selfServiceDetails: null,
-                advanceStage: () => true
-            }
-        });
-    };
+    var sandbox = null;
 
     Vue.use(VueI18n);
     Vue.use(BootstrapVue);
+
+    beforeEach(function () {
+        sandbox = Sinon.sandbox.create();
+
+        sandbox.stub(Captcha, 'mounted').callsFake(function () {});
+    });
+
+    afterEach(function () {
+        sandbox.restore();
+    });
 
     const i18n = new VueI18n({
         locale: 'en',
@@ -26,24 +28,46 @@ describe('Captcha.vue', () => {
     });
 
     it('Captcha component loaded', () => {
-        const wrapper = mountWrapper();
+        const wrapper = mount(Captcha, {
+            i18n,
+            propsData: {
+                apiType: 'username',
+                selfServiceDetails: null,
+                advanceStage: () => true
+            }
+        });
 
         expect(wrapper.name()).to.equal('Captcha');
     });
 
     it('Captcha handleCaptchaCallback sets proper value', () => {
-        const wrapper = mountWrapper();
+        const wrapper = mount(Captcha, {
+            i18n,
+            propsData: {
+                apiType: 'username',
+                selfServiceDetails: null,
+                advanceStage: () => true
+            }
+        });
 
         wrapper.vm.handleCaptchaCallback('test');
         expect(wrapper.vm.recaptchaResponse).to.equal('test');
     });
 
     it('Captcha getData() returns proper output', () => {
+        const wrapper = mount(Captcha, {
+            i18n,
+            propsData: {
+                apiType: 'username',
+                selfServiceDetails: null,
+                advanceStage: () => true
+            }
+        });
+
         var expectedResult = {
             response: 'test',
             'g-recaptcha-response': 'test'
         };
-        const wrapper = mountWrapper();
 
         wrapper.vm.handleCaptchaCallback('test');
 
