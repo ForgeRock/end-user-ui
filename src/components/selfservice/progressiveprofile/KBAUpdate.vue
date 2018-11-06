75d38e20507e2ab9a7a8dd31729820af5ea01935
Javier Quiroz <javier.quiroz@forgerock.com>
2018-11-06 17:10:07 +0000

    OPENIDM-12030 Fixes progressive profile KBA Update stage to not show questions as "undefined"

--- src/components/selfservice/progressiveprofile/KBAUpdate.vue
+++ src/components/selfservice/progressiveprofile/KBAUpdate.vue
@@ -1,113 +1,45 @@
 <template>
-    <div>
-        <b-form-group label-text-align="left" class="mb-0"
-            v-for="(answer, key) in answers" :key="key">
-
-            <b-form-select class="mb-3"
-                v-model="selected[key].selected"
-                :options="options" ></b-form-select>
-
-            <fr-floating-label-input class="mb-3" type="text"
-                v-if="selected[key].selected === customIndex"
-                v-model.trim="answer.customQuestion"
-                :fieldName="$t('common.user.kba.question').toLowerCase()"
-                :label="$t('common.user.kba.question')"
-                :validateRules="'required'"></fr-floating-label-input>
-
-            <fr-floating-label-input class="mb-3" type="text"
-                v-model.trim="answer.answer"
-                :fieldName="$t('common.user.kba.answer').toLowerCase()"
-                :label="$t('common.user.kba.answer')"
-                :validateRules="'required'"></fr-floating-label-input>
-
-            <hr v-if="key !== answers.length - 1">
-        </b-form-group>
-    </div>
+    <fr-kba-form-group ref="kbaFormGroup" :selfServiceDetails="selfServiceDetails"></fr-kba-form-group>
 </template>
 
 <script>
-    import _ from 'lodash';
-    import FloatingLabelInput from '@/components/utils/FloatingLabelInput';
+    import KBADefinitionFormGroup from '@/components/selfservice/common/KBADefinitionFormGroup';
 
     /**
-     * @description Selfservice stage for progressive profile, handles the case where a resourse is requested to change their KBA questions
+     * @description Selfservice stage for progressive profile, handles the case where a resource is requested to change their KBA questions
      *
      **/
     export default {
         name: 'KBA-Update-Stage',
         props: {
-            selfServiceDetails: { required: true }
-        },
-        $_veeValidate: {
-            validator: 'new'
+            selfServiceDetails: { required: true },
+            inline: {
+                required: false,
+                default: false
+            }
         },
         components: {
-            'fr-floating-label-input': FloatingLabelInput
+            'fr-kba-form-group': KBADefinitionFormGroup
         },
-        inject: ['$validator'],
         data () {
-            let locale = this.$i18n.locale,
-                numberOfQuestions = _.toString(this.selfServiceDetails.requirements.properties.kba.minItems),
-                answers = _.times(numberOfQuestions, (n) => { return { answer: null, questionId: null, customQuestion: null }; }),
-                selected = _.times(numberOfQuestions, () => { return {selected: null}; }),
-                options = _.map(this.selfServiceDetails.requirements.properties.kba.questions, (question) => {
-                    return { value: question.id, text: question.question[locale], disabled: false };
-                }),
-                customIndex = options.length + 1;
-
-            // 'placeholder' should be first item in options array and 'custom' should be last
-            options.unshift({ value: null, text: this.$t('common.user.kba.selectQuestion'), disabled: true });
-            options.push({ value: customIndex, text: this.$t('common.user.kba.custom'), disabled: false });
-
-            return {
-                selected: selected,
-                options: options,
-                answers: answers,
-                customIndex: customIndex
-            };
-        },
-        watch: {
-            selected: {
-                handler (value) {
-                    // create array of selected options that aren't custom
-                    let toDisable = _.map(this.selected, (s) => {
-                        if (s.selected !== null && s.selected !== this.customIndex) {
-                            return s.selected;
-                        }
-                    });
-
-                    // set any [toDisable] option to disabled
-                    _.each(this.options, (o) => {
-                        if (_.includes(toDisable, o.value) || o.value === null) {
-                            o.disabled = true;
-                        } else {
-                            o.disabled = false;
-                        }
-                    });
-                },
-                deep: true
-            }
+            return {};
         },
         methods: {
             getData () {
-                let saveArray = _.map(this.selected, (s, index) => {
-                    let couplet = {};
+                return this.$refs.kbaFormGroup.getData();
+            },
 
-                    // if custom question
-                    if (s.selected === this.customIndex) {
-                        couplet.customQuestion = this.answers[index].customQuestion;
-                    } else {
-                        couplet.questionId = s.selected;
+            save () {
+                /* istanbul ignore next */
+                this.isValid().then((valid) => {
+                    if (valid) {
+                        this.$emit('advanceStage', this.getData());
                     }
-                    couplet.answer = this.answers[index].answer;
-                    return couplet;
                 });
-
-                return { kba: saveArray };
             },
             isValid () {
                 /* istanbul ignore next */
-                return this.$validator.validateAll();
+                return this.$refs.kbaFormGroup.isValid();
             }
         }
     };
--- src/components/selfservice/registration/KBASecurityAnswerDefinitionStage.vue
+++ src/components/selfservice/registration/KBASecurityAnswerDefinitionStage.vue
@@ -4,37 +4,15 @@
 
         <p class="text-center">{{$t('common.user.kba.description')}}</p>
 
-        <b-form-group label-text-align="left" class="mb-0"
-            v-for="(answer, key) in answers" :key="key">
-
-            <b-form-select class="mb-3"
-                v-model="answer.questionId"
-                :options="options"></b-form-select>
-
-            <fr-floating-label-input class="mb-3" type="text"
-                v-if="answer.questionId === customIndex"
-                v-model.trim="answer.customQuestion"
-                :fieldName="$t('common.user.kba.question').toLowerCase() + key"
-                :label="$t('common.user.kba.question')"
-                :validateRules="{required: true, unique_question: getDuplicates(key)}"></fr-floating-label-input>
-
-            <fr-floating-label-input class="mb-3" type="text"
-                v-model.trim="answer.answer"
-                :fieldName="$t('common.user.kba.answer').toLowerCase() + key"
-                :label="$t('common.user.kba.answer')"
-                :validateRules="'required'"></fr-floating-label-input>
-
-            <hr v-if="key !== answers.length - 1">
-        </b-form-group>
+        <fr-kba-form-group ref="kbaFormGroup" :selfServiceDetails="selfServiceDetails"></fr-kba-form-group>
 
         <b-button v-if="!inline" @click="save" :block="true" size="lg" variant="primary">{{$t("pages.selfservice.registration.completeRegistration")}}</b-button>
     </b-form>
 </template>
 
 <script>
-    import _ from 'lodash';
     import HorizontalRule from '@/components/utils/HorizontalRule';
-    import FloatingLabelInput from '@/components/utils/FloatingLabelInput';
+    import KBADefinitionFormGroup from '@/components/selfservice/common/KBADefinitionFormGroup';
 
     /**
      * @description Selfservice stage that handles the initial KBA during registration functions the same in allinone
@@ -49,55 +27,18 @@
                 default: false
             }
         },
-        $_veeValidate: {
-            validator: 'new'
-        },
         components: {
             'fr-horizontal-rule': HorizontalRule,
-            'fr-floating-label-input': FloatingLabelInput
+            'fr-kba-form-group': KBADefinitionFormGroup
         },
-        inject: ['$validator'],
         data () {
-            let kba = this.selfServiceDetails.requirements.properties.kba,
-                answers = _.times(kba.minItems, () => ({ answer: null, questionId: null, customQuestion: null })),
-                customIndex = 'custom';
-
-            return { kba, answers, customIndex };
-        },
-        computed: {
-            predefinedQuestionOptions () {
-                return _.map(this.kba.questions, (question) => {
-                    let value = question.id,
-                        text = question.question[this.$i18n.locale] || question.question[this.$i18n.fallbackLocale],
-                        disabled = !_.isUndefined(_.find(this.answers, {questionId: question.id}));
-
-                    return { value, text, disabled };
-                });
-            },
-            predefinedQuestionText () {
-                return this.predefinedQuestionOptions.map((question) => question.text);
-            },
-            options () {
-                let placeholder = { value: null, text: this.$t('common.user.kba.selectQuestion'), disabled: true },
-                    customQuestionOption = { value: 'custom', text: this.$t('common.user.kba.custom'), disabled: false };
-
-                return Array.concat(placeholder, this.predefinedQuestionOptions, customQuestionOption);
-            }
+            return {};
         },
         methods: {
             getData () {
-                return {
-                    kba: this.answers.map((answer) => {
-                        return _.omit(answer, answer.questionId === this.customIndex ? 'questionId' : 'customQuestion');
-                    })
-                };
+                return this.$refs.kbaFormGroup.getData();
             },
-            getDuplicates (key) {
-                return _.clone(this.answers)
-                    .map((answer) => answer.customQuestion)
-                    .filter((question, index) => index !== key && question)
-                    .concat(this.predefinedQuestionText);
-            },
+
             save () {
                 /* istanbul ignore next */
                 this.isValid().then((valid) => {
@@ -108,19 +49,8 @@
             },
             isValid () {
                 /* istanbul ignore next */
-                return this.$validator.validateAll();
+                return this.$refs.kbaFormGroup.isValid();
             }
-        },
-        created () {
-            /* istanbul ignore next */
-            this.$validator.extend('unique_question', {
-                getMessage: (field, exclusions) => this.$t('common.user.kba.notUnique'),
-                validate: (value, exclusions) => {
-                    const trimToLower = (string) => _.trim(_.toLower(string));
-
-                    return !_.includes(_.map(exclusions, trimToLower), trimToLower(value));
-                }
-            });
         }
     };
 </script>
