b7e68034b383ee2d9415f24f084d84f2af9fd18d
jason.browne <jason.browne@forgerock.com>
2019-04-07 13:06:59 -0700

    OPENIDM-12698 Fix paths to not be replative to dist generated app scripts

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '',
     proxyTable: {
       '/openidm': {
         target: 'http://localhost:8080/openidm',
