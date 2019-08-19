ca24d2826ac379f980cbd59380bee05f8aa7a5d5
oliver.bradley <oliver.bradley@forgerock.com>
2019-08-19 14:44:09 -0700

    OPENIDM-13191 Enduser-UI - fix signup page to correctly render html when terms & conditions are shown

--- src/components/selfservice/registration/TermsAndConditions.vue
+++ src/components/selfservice/registration/TermsAndConditions.vue
@@ -6,14 +6,14 @@
 
         <b-modal id="termsModal" hide-footer :title="$t('pages.selfservice.registration.termsAndConditions.title')">
             <div class="d-block text-center">
-                <p>{{selfServiceDetails.requirements.terms}}</p>
+                <p v-html="selfServiceDetails.requirements.terms">{{selfServiceDetails.requirements.terms}}</p>
             </div>
         </b-modal>
     </div>
     <div v-else>
         <h3>{{$t("pages.selfservice.registration.termsAndConditions.title")}}</h3>
         <div class="d-block text-center">
-            <p>{{selfServiceDetails.requirements.terms}}</p>
+            <p v-html="selfServiceDetails.requirements.terms">{{selfServiceDetails.requirements.terms}}</p>
         </div>
 
         <b-button v-if="inline === false" @click="save" :block="true" size="lg" variant="primary">
