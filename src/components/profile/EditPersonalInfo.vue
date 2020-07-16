195322d079ef98bb4fc9dbc479be85d38b64f114
Chuck Masucci <chuck.masucci@forgerock.com>
2020-07-16 14:38:43 -0700

    OPENIDM-15024 Fixed email address not allowing form to submit

--- src/components/profile/EditPersonalInfo.vue
+++ src/components/profile/EditPersonalInfo.vue
@@ -10,42 +10,37 @@
         <b-container>
             <b-row>
                 <b-col sm="8" offset-sm="2">
-                    <b-form style="flex-direction: column;" v-if="formFields.length > 0" class="mb-3 fr-edit-personal-form" name="edit-personal-form">
-                        <template v-for="(field, index) in formFields">
-                            <b-form-group style="min-width: 200px;" :key="index" v-if="field.type === 'string' || field.type === 'number'">
-                                <label :for="field.title">{{field.title}}</label>
-                                <small v-if="!field.required" class="text-muted ml-1">{{$t('pages.profile.editProfile.optional')}}</small>
+                    <ValidationObserver ref="observer" slim>
+                        <b-form style="flex-direction: column;" v-if="formFields.length > 0" class="mb-3 fr-edit-personal-form" name="edit-personal-form">
+                            <template v-for="(field, index) in displayFields">
+                                <b-form-group style="min-width: 200px;" :key="index" v-if="field.type === 'string' || field.type === 'number'">
+                                    <label :for="field.title">{{field.title}}</label>
+                                    <small v-if="!field.required" class="text-muted ml-1">{{$t('pages.profile.editProfile.optional')}}</small>
+                                    <ValidationProvider :rules="`${field.required ? 'required' : ''}`"  :name="field.name" v-slot="validationContext">
+                                        <b-input
+                                            :name="field.name"
+                                            :type="field.type === 'string' ? 'text' : field.type"
+                                            :state="getValidationState(validationContext)"
+                                            v-model.trim="formFields[index].value" />
+                                        <b-form-invalid-feedback>{{ validationContext.errors[0] }}</b-form-invalid-feedback>
+                                    </ValidationProvider>
+                                </b-form-group>
 
-                                <input v-if="field.type === 'string'" v-validate="field.required ? 'required' : ''" data-vv-validate-on="submit"
-                                       :name="field.name"
-                                       :type="field.type"
-                                       :class="[{'is-invalid': errors.has(field.name)}, 'form-control']"
-                                       :data-vv-as="field.title"
-                                       v-model.trim="formFields[index].value">
+                                <!-- for boolean values -->
+                                <b-form-group :key="index" v-if="field.type === 'boolean'">
+                                    <div class="d-flex flex-column">
+                                        <label class="mr-auto" :for="field.title">{{field.title}}</label>
 
-                                <input v-else v-validate="field.required ? 'required' : ''" data-vv-validate-on="submit"
-                                       :name="field.name"
-                                       :type="field.type"
-                                       :class="[{'is-invalid': errors.has(field.name)}, 'form-control']"
-                                       :data-vv-as="field.title"
-                                       v-model.number="formFields[index].value">
-                                <fr-validation-error :validatorErrors="errors" :fieldName="field.name"></fr-validation-error>
-                            </b-form-group>
-
-                            <!-- for boolean values -->
-                            <b-form-group :key="index" v-if="field.type === 'boolean'">
-                                <div class="d-flex flex-column">
-                                    <label class="mr-auto" :for="field.title">{{field.title}}</label>
-
-                                    <div class="mr-auto">
-                                        <toggle-button class="mt-2 p-0 fr-toggle-primary"
-                                                       :height="28"
-                                                       :width="56"
-                                                       :sync="true"
-                                                       :cssColors="true"
-                                                       :labels="{checked: $t('common.form.yes'), unchecked: $t('common.form.no')}"
-                                                       :value="formFields[index].value"
-                                                       @change="formFields[index].value = !formFields[index].value"/>
+                                        <div class="mr-auto">
+                                            <toggle-button class="mt-2 p-0 fr-toggle-primary"
+                                                :height="28"
+                                                :width="56"
+                                                :sync="true"
+                                                :cssColors="true"
+                                                :labels="{checked: $t('common.form.yes'), unchecked: $t('common.form.no')}"
+                                                :value="formFields[index].value"
+                                                @change="formFields[index].value = !formFields[index].value"/>
+                                        </div>
                                     </div>
                                 </div>
                             </b-form-group>
@@ -54,6 +49,7 @@
                     <template v-else>
                         <h3 class="text-center">{{$t('pages.profile.editProfile.noFields')}}</h3>
                     </template>
+                    </ValidationObserver>
                 </b-col>
             </b-row>
         </b-container>
@@ -68,59 +64,67 @@
 </template>
 
 <script>
-    import _ from 'lodash';
-    import ValidationError from '@/components/utils/ValidationError';
-    import ResourceMixin from '@/components/utils/mixins/ResourceMixin';
+import _ from 'lodash';
+import ResourceMixin from '@/components/utils/mixins/ResourceMixin';
 
-    /**
-     * @description Displays a users profile, auto generates fields based off of resource schema. Currently only displays strings, numbers and booleans. In the case of a policy
-     * save error it will highlight the appropriate field and display a policy error. For custom profile changes (e.g. adding a dropdown) this would be the primary file to add these
-     * adjustments.
-     *
-     */
-    export default {
-        name: 'Edit-Personal-Info',
-        mixins: [
-            ResourceMixin
-        ],
-        components: {
-            'fr-validation-error': ValidationError
-        },
-        $_veeValidate: {
-            validator: 'new'
-        },
-        props: {
-            schema: { type: Object, required: true },
-            profile: { type: Object, required: true }
-        },
-        data () {
-            return {
-                formFields: [],
-                originalFormFields: [],
-                title: this.$t('pages.profile.editProfile.userDetailsTitle')
-            };
-        },
-        methods: {
-            generateFormFields () {
-                let { order, properties, required } = this.schema,
-                    filteredOrder = _.filter(order, (propName) => {
-                        return properties[propName].viewable &&
-                            properties[propName].userEditable &&
+/**
+ * @description Displays a users profile, auto generates fields based off of resource schema. Currently only displays strings, numbers and booleans. In the case of a policy
+ * save error it will highlight the appropriate field and display a policy error. For custom profile changes (e.g. adding a dropdown) this would be the primary file to add these
+ * adjustments.
+ *
+ */
+export default {
+    name: 'Edit-Personal-Info',
+    mixins: [
+        ResourceMixin
+    ],
+    components: {},
+    props: {
+        schema: { type: Object, required: true },
+        profile: { type: Object, required: true },
+        autoOpen: { type: Boolean, required: false, default: false }
+    },
+    data () {
+        return {
+            formFields: [],
+            originalFormFields: [],
+            title: this.$t('pages.profile.editProfile.userDetailsTitle')
+        };
+    },
+    mounted () {
+        if (this.autoOpen) {
+            this.$root.$emit('bv::show::modal', 'userDetailsModal');
+        }
+    },
+    computed: {
+        displayFields () {
+            let { properties } = this.schema,
+                filterFields = _.filter(this.formFields, (field) => {
+                    return properties[field.name].userEditable;
+                });
+            return filterFields;
+        }
+    },
+    methods: {
+        generateFormFields () {
+            let { order, properties, required } = this.schema,
+                filteredOrder = _.filter(order, (propName) => {
+                    return properties[propName].viewable &&
                             properties[propName].type !== 'array' &&
                             properties[propName].type !== 'object';
-                    }),
-                    formFields = _.map(filteredOrder, (name) => {
-                        return {
-                            name: name,
-                            key: name,
-                            title: properties[name].title,
-                            value: this.profile[name] || null,
-                            type: properties[name].type,
-                            required: _.includes(required, name)
-                        };
-                    });
+                }),
+                formFields = _.map(filteredOrder, (name) => {
+                    return {
+                        name: name,
+                        key: name,
+                        title: properties[name].title,
+                        value: this.profile[name] || null,
+                        type: properties[name].type,
+                        required: _.includes(required, name)
+                    };
+                });
 
-                return formFields;
+            return formFields;
             },
             hideModal () {
                 this.$refs.fsModal.hide();
