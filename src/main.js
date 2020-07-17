89e9b6fb0916f06fa6e48afaa606c1848ad873db
Andrew Hertel <andrew.hertel@forgerock.com>
2020-07-17 13:58:47 -0700

    OPENIDM-14583 Allow \ and / to follow | character in login

--- src/main.js
+++ src/main.js
@@ -301,8 +301,7 @@ Vue.mixin({
             /* istanbul ignore next */
             let encoded = encodeURIComponent(headerValue)
                 .replace(/['()]/g, escape)
-                .replace(/\*/g, '%2A')
-                .replace(/%(?:7C|60|5E)/g, unescape);
+                .replace(/\*/g, '%2A');
             /* istanbul ignore next */
             return encoded === headerValue ? headerValue : "UTF-8''" + encoded;
         }
