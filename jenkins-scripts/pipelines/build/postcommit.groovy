8afc1d976e6ce57097d0707830c3303d8ab8726d
Elma Blum <elma.blum@forgerock.com>
2021-02-11 14:40:06 -0800

    RELENG-1105 Move Whitesource scan to product pipeline

--- jenkins-scripts/pipelines/build/postcommit.groovy
+++ jenkins-scripts/pipelines/build/postcommit.groovy
@@ -5,6 +5,8 @@
 //====================================
 
 import com.forgerock.pipeline.Build
+import com.forgerock.pipeline.whitesource.ScanResult
+import com.forgerock.pipeline.scp.ScpHost
 import java.text.SimpleDateFormat
 
 def build() {
@@ -52,6 +54,58 @@ def build() {
           sh "mvn -B -e -U clean deploy -Psource-copyright,thirdpartylicensing -Dci.scm.revision=${SHORT_GIT_COMMIT}" +
                   " -Dwhitesource.product.key=${env.WS_PRODUCT_KEY} -Dwhitesource.user.key=${env.WS_USER_KEY}"
         }
+      }
+    }
+
+    stage ('Whitesource Scan') {
+      try {
+        def repoName = scmUtils.getRepoName()
+        def projectVersion = env.BRANCH_NAME
+        def mavenJvmOptions = '-XX:MaxPermSize=256m -Xmx2g'
+
+        // Remove spaces from branch name, and build a clean branch name for the folder and log names (without /)
+        def gitBranchNameForLogAndFolder = projectVersion.replaceAll("\\s","")
+                .replaceAll("/","-")
+
+        // Target security folder on Abondance FR
+        def scpHost = ScpHost.ABONDANCE_FR
+        def scpRemotePathRoot = "security/whitesource/${repoName}/${gitBranchNameForLogAndFolder}/${SHORT_GIT_COMMIT}"
+        def scpRemotePath = "/export/${scpRemotePathRoot}"
+        scp.checkCanCopyToPath(scpHost, scpRemotePath, false)
+        def riskReportName = 'whitesource-scan-result.pdf'
+
+        def productToken = whitesourceUtils.getWhitesourceToken(repoName, projectVersion)
+        ScanResult whitesourceScanResult
+
+        withEnv(["JAVA_HOME=" + tool("JDK${javaVersion}"),
+                 "MAVEN_OPTS=${mavenBuildOptions}",
+                 "PATH+MAVEN=" + tool("Maven ${mavenVersion}") + "/bin"]) {
+
+          def scanArgs = [
+                  productToken:   productToken,
+                  projectVersion: projectVersion,
+          ]
+
+          whitesourceScanResult = whitesourceUtils.performWhitesourceScan(scanArgs, riskReportName)
+        }
+
+        if (!whitesourceScanResult.scanPassed) {
+          scpRemotePath += '_CVE'
+        }
+
+        scp.scpToRemoteDirectory(riskReportName, scpHost, scpRemotePath)
+        scp.scpToRemoteDirectory('whitesource/*', scpHost, "${scpRemotePath}/log")
+
+        echo "Whitesource scan result: ${whitesourceScanResult.resultDescription}"
+
+        if (whitesourceScanResult.scanPassed) {
+          currentBuild.result = 'SUCCESS'
+        } else {
+          currentBuild.result = 'FAILURE'
+        }
+      } catch (exception) {
+        emailUtils.alertReleaseEngineeringAboutExternalServiceIssue('WhiteSource', exception.message)
+        currentBuild.result = 'UNSTABLE'
       }
     }
 
