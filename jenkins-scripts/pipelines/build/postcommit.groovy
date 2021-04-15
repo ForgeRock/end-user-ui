3ee8c990e58acabeb9fece86031d7aea7b528758
Bruno Lavit <bruno.lavit@forgerock.com>
2021-04-15 14:24:38 +0200

    RELENG-1181 Use the new getWhiteSourceToken method to get the product token on the fly

--- jenkins-scripts/pipelines/build/postcommit.groovy
+++ jenkins-scripts/pipelines/build/postcommit.groovy
@@ -47,11 +47,11 @@ def build() {
                "MAVEN_OPTS=${mavenBuildOptions}",
                "PATH+MAVEN=" + tool("Maven ${mavenVersion}") + "/bin"]) {
         withCredentials([
-                string(credentialsId: 'whitesource-key-openidm-enduser', variable: 'WS_PRODUCT_KEY'),
                 string(credentialsId: 'whitesource-ci-user-key', variable: 'WS_USER_KEY'),
         ]) {
+          def whitesourceProductToken = whitesourceUtils.getWhitesourceToken(scmUtils.getRepoName(), env.BRANCH_NAME)
           sh "mvn -B -e -U clean deploy -Psource-copyright,thirdpartylicensing -Dci.scm.revision=${SHORT_GIT_COMMIT}" +
-                  " -Dwhitesource.product.key=${env.WS_PRODUCT_KEY} -Dwhitesource.user.key=${env.WS_USER_KEY}"
+                  " -Dwhitesource.product.key=${whitesourceProductToken} -Dwhitesource.user.key=${env.WS_USER_KEY}"
         }
       }
     }
--- jenkins-scripts/pipelines/build/pr.groovy
+++ jenkins-scripts/pipelines/build/pr.groovy
@@ -42,11 +42,11 @@ def build() {
                "PATH+MAVEN=" + tool("Maven ${mavenVersion}") + "/bin"]) {
 
         withCredentials([
-                string(credentialsId: 'whitesource-key-openidm-enduser', variable: 'WS_PRODUCT_KEY'),
                 string(credentialsId: 'whitesource-ci-user-key', variable: 'WS_USER_KEY'),
         ]) {
+          def whitesourceProductToken = whitesourceUtils.getWhitesourceToken(scmUtils.getRepoName(), env.CHANGE_TARGET)
           sh "mvn -B -e -U clean verify -Psource-copyright,thirdpartylicensing -Dci.scm.revision=${SHORT_GIT_COMMIT}" +
-                  " -Dwhitesource.product.key=${env.WS_PRODUCT_KEY} -Dwhitesource.user.key=${env.WS_USER_KEY}"
+                  " -Dwhitesource.product.key=${whitesourceProductToken} -Dwhitesource.user.key=${env.WS_USER_KEY}"
         }
       }
     }
