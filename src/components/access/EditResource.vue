0f566847eccaa56416a43bb3a9ea3b6a86d98b74
Oliver Bradley <oliver.bradley@forgerock.com>
2023-07-13 14:05:22 -0700

    OPENIDM-18497 - enduser UI - Property Value Substitution fails to reflect boolean value in UI

--- src/components/access/EditResource.vue
+++ src/components/access/EditResource.vue
@@ -1,5 +1,5 @@
 <!--
-Copyright (c) 2020 ForgeRock. All rights reserved.
+Copyright (c) 2020-2023 ForgeRock. All rights reserved.
 
 This software may be modified and distributed under the terms
 of the MIT license. See the LICENSE file for details.
@@ -268,7 +268,7 @@ export default {
                     tempProp.key = createPriv.attribute;
 
                     // Try and do some primary detection for a display name
-                    if (createPriv.attrubute !== '_id' && this.displayNameField.length === 0) {
+                    if (createPriv.attribute !== '_id' && createPriv.attribute !== 'password' && this.displayNameField.length === 0) {
                         this.displayNameField = createPriv.attribute;
                     }
 
