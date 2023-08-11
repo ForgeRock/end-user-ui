8b0f3fb41be55648bd80c6180113005987a45948
Oliver Bradley <oliver.bradley@forgerock.com>
2023-08-11 07:34:34 -0700

    OPENIDM-18617 - End-user UI not redirecting to the authentication page after session timeout

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright (c) 2022 ForgeRock. All rights reserved.
+ * Copyright (c) 2022-2023 ForgeRock. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
@@ -225,10 +225,7 @@ Vue.mixin({
             }, (error) => {
                 if (error.response && error.response.data && error.response.data.code === 401) {
                     if (this.$route.name !== 'Login') {
-                        ApplicationStore.setLoginRedirect({
-                            name: this.$route.name,
-                            params: this.$route.params
-                        });
+                        this.logoutUser();
                     }
 
                     return Promise.reject(error);
