5e05a0f68325bb9eabd6da53ef115dccd80fffa7
Robin Banbury <robin.banbury@forgerock.com>
2023-10-11 22:23:54 +0200

    RELENG-2198 Replace Whitesource credentials with Mend ones

--- jenkins-scripts/pipelines/build/pr.groovy
+++ jenkins-scripts/pipelines/build/pr.groovy
@@ -42,11 +42,11 @@ def build() {
                "PATH+MAVEN=" + tool("Maven ${mavenVersion}") + "/bin"]) {
 
         withCredentials([
-                string(credentialsId: 'whitesource-ci-user-key', variable: 'WS_USER_KEY'),
+                string(credentialsId: 'mend-ci-user-key', variable: 'MEND_USER_KEY'),
         ]) {
           def whitesourceProductToken = mendUtils.getProductToken(scmUtils.getRepoName(), env.CHANGE_TARGET)
           sh "mvn -B -e -U clean verify -Psource-copyright,thirdpartylicensing -Dci.scm.revision=${SHORT_GIT_COMMIT}" +
-                  " -Dmend.product.key=${whitesourceProductToken} -Dmend.user.key=${env.WS_USER_KEY}"
+                  " -Dmend.product.key=${whitesourceProductToken} -Dmend.user.key=${env.MEND_USER_KEY}"
         }
       }
     }
